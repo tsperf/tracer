13adf7b86c017c1a34eb44ebc1a359b8669a1db0
typeholes <typeholes@protonmail.net>
2024-05-12 06:08:17 -0400

    builds with rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,7 @@
+import commonjs from '@rollup/plugin-commonjs'
 import { defineConfig } from 'rollup'
 import esbuild from 'rollup-plugin-esbuild'
+import json from '@rollup/plugin-json'
 import resolve from '@rollup/plugin-node-resolve'
 import { raw } from './build/raw.mjs'
 
@@ -7,5 +9,5 @@ export default defineConfig({
   external: ['vscode', 'typescript', /^node:.*/],
   input: ['./src/index.ts'],
   output: { format: 'cjs', dir: 'dist' },
-  plugins: [esbuild(), raw(), resolve()],
+  plugins: [esbuild(), raw(), resolve(), commonjs(), json()],
 })
